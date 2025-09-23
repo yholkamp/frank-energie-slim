b528b845b5e09e46d71c37cc02ee1132f3aa197b
Yorick Holkamp <yorick@nextpulse.nl>
2025-09-23 14:15:14 +0200

    Query patch

--- custom_components/frank_energie_slim/api.py
+++ custom_components/frank_energie_slim/api.py
@@ -143,7 +143,7 @@ class FrankEnergie:
                         sessions {
                             cumulativeResult
                             date
-                            tradingResult
+                            result
                         }
                     }
                 }
